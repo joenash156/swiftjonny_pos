a2ef223c6dc5c331480cb3a711b49f9b0d5a4614
joenash156 <joenash156@gmail.com>
2026-01-14 05:30:22 +0000

    done with the get all cashiers controller

--- backend/src/controllers/adminControllers.ts
+++ backend/src/controllers/adminControllers.ts
@@ -5,5 +5,45 @@ import { ResultSetHeader, RowDataPacket } from "mysql2";
 
 // controller to get all cashiers
 export const getAllCashiers = async (req: Request, res: Response): Promise<void> => {
-  
+  try {
+    // make sure only admins can access the controller/endpoint/route
+    const userRole = req.user.role;
+
+    // check if role exists
+    if(!userRole || userRole !== "admin") {
+      res.status(403).json({
+        success: false,
+        error: "Unauthorized users and/or cashiers cannot view all cashiers"
+      });
+      return;
+    }
+
+    const { is_approved } = req.query;
+
+    const [cashiers] = await db.query<RowDataPacket[]>("SELECT firstname, lastname, othername, email, phone, other_phone, is_approved, role, last_login_at, is_profile_complete, created_at FROM users WHERE role = cashier");
+
+    if(cashiers.length === 0) {
+      res.status(200).json({
+        success: true,
+        counts: cashiers.length,
+        message: "No cashier found!✅"
+      });
+      return;
+    }
+
+    res.status(200).json({
+      success: true,
+      counts: cashiers.length,
+      message: "All cashiers found!✅",
+      cashiers
+    });
+    return;
+
+  } catch(err: unknown) {
+      console.error("Failed fetching all cashiers: ", err);
+      res.status(500).json({
+        success: false,
+        error: "Failed fetching all cashiers. Internal server error!"
+      })
+  }
 }
