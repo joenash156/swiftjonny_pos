690717ae2e3a4118f611c3e58faac3df358b7a97
joenash156 <joenash156@gmail.com>
2026-02-05 23:24:54 +0000

    made a change

--- backend/src/routes/analytics.routes.ts
+++ backend/src/routes/analytics.routes.ts
@@ -1,29 +1,9 @@
 import express, { Router } from "express";
-import { requireAdmin } from "../middlewares/role.middleware";
 import { requireAuth } from "../middlewares/auth.middleware";
-import { approveCashier, deleteCashier, disableCashier, getAllCashiers, getCashierById, updateUserRole } from "../controllers/admin.controllers";
-import { validateUUID } from "../middlewares/uuidValidation.middleware";
 
 const router: Router = express.Router();
 
-// router to get all cashiers (only executed by admins)
-router.get("/cashiers", requireAuth, requireAdmin, getAllCashiers);
-
-// router to get a cashier by id (only executed by admins)
-router.get("/cashier/:id", requireAuth, requireAdmin, validateUUID, getCashierById);
-
-// router to approve a cashier by id (only executed by admins)
-router.patch("/cashier/:id/approve", requireAuth, requireAdmin, validateUUID, approveCashier);
-
-// router to disable/disprove a cashier by id (only executed by admins)
-router.patch("/cashier/:id/disable", requireAuth, requireAdmin, validateUUID, disableCashier);
-
-// router to disable/disprove a cashier by id (only executed by admins)
-router.delete("/cashier/:id/delete", requireAuth, requireAdmin, validateUUID, deleteCashier);
-
-// router to update a user's role (only executed by admins)
-router.patch("/user/:id/update_role", requireAuth, requireAdmin, validateUUID, updateUserRole);
-
-
+// router to get analytics summary
+router.get("/summary", requireAuth)
 
 export default router;
