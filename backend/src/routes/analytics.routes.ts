959901b5d8e98638f332f6cfde61623a7e785cc4
joenash156 <joenash156@gmail.com>
2026-02-06 23:21:01 +0000

    setting up frontend

--- backend/src/routes/analytics.routes.ts
+++ backend/src/routes/analytics.routes.ts
@@ -3,7 +3,7 @@ import { requireAuth } from "../middlewares/auth.middleware";
 
 const router: Router = express.Router();
 
-// router to get analytics summary
+// router to get analytics summarys
 router.get("/summary", requireAuth)
 
 export default router;
