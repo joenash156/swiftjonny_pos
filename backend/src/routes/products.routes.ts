6cdb952098e11ec190d562af9f07aa48ab9b7074
joenash156 <joenash156@gmail.com>
2026-02-13 23:34:46 +0000

    have some todo

--- backend/src/routes/products.routes.ts
+++ backend/src/routes/products.routes.ts
@@ -3,11 +3,13 @@ import { requireAuth } from "../middlewares/auth.middleware";
 import { requireAdmin } from "../middlewares/role.middleware";
 import { createProduct, deleteProduct, getAllProducts, getProductById, updateProduct } from "../controllers/products.controllers";
 import { validateUUID } from "../middlewares/uuidValidation.middleware";
+import { requireUploadType } from "../middlewares/requireUploadType.middleware";
+import upload from "../middlewares/uploads.middleware";
 
 const router: Router = express.Router();
 
 // router to create/insert a product (only executed by admins)
-router.post("/create", requireAuth, requireAdmin, createProduct);
+router.post("/create", requireAuth, requireAdmin, requireUploadType("product"), upload.single("product"), createProduct);
 
 // router to get all products
 router.get("/get_all", requireAuth, getAllProducts);
@@ -15,8 +17,8 @@ router.get("/get_all", requireAuth, getAllProducts);
 // router to get a product by id
 router.get("/:id", requireAuth, validateUUID, getProductById);
 
-// router to update a product (only executed by admins)
-router.patch("/:id/update", requireAuth, requireAdmin, validateUUID, updateProduct)
+// router to update a product (only executed by admins) Todo: [Make changes]
+router.patch("/:id/update", requireAuth, requireAdmin, validateUUID, requireUploadType("product"), upload.single("product"), updateProduct)
 
 // router to delete a product (only executed by admins)
 router.delete("/:id/delete", requireAuth, requireAdmin, validateUUID, deleteProduct)
